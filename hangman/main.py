ee5c3f3412764573a1a4c881d9fdc5b90691dfee
nwkwok <nwkwok@gmail.com>
2020-12-31 00:26:01 -0800

    finish hangman script

--- hangman/main.py
+++ hangman/main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+import random
+from hangman_art import logo, stages
+from hangman_words import word_list
+# import os
+# from replit import clear
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print(logo)
+chosen_word = random.choice(word_list)
+display = []
+
+for _ in range(len(chosen_word)):
+    display += '_'
+
+end_of_game = False
+lives = 6
+wrong_guess = []
+
+while not end_of_game:
+    guess = input('Guess a letter\n').lower()
+    print(f'You guessed the letter "{guess}"')
+    # os.system('clear')
+    # clear()
+
+    if guess in display:
+        print('You already guessed this letter. Try a different letter')
+
+    for position in range(len(chosen_word)):
+        if guess == chosen_word[position]:
+            display[position] = guess
+
+    if '_' not in display:
+        print('Congraulations! You win!')
+        end_of_game = True
+    if len(guess) == 0:
+        print('To play, you gotta guess a letter... duh!')
+    elif len(guess) > 1:
+        print('Woah... woah... one letter at a time please!')
+    elif guess in wrong_guess:
+        print('You already guessed that letter... and it was wrong! Try a different one.')
+    elif guess not in chosen_word:
+        lives -= 1
+        wrong_guess += guess
+        if lives == 1:
+            print('You have one life left... make it count!')
+        elif lives == 0:
+            end_of_game = True
+            print("You lose!")
+        else:
+            print(f'You have {lives} lives left')
+
+    print(stages[lives])
+    print(display)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
