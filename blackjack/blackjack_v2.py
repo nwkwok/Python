a09b0600e4a1ad32edb14fdbabf87a9aaa35b757
nwkwok <nwkwok@gmail.com>
2021-01-05 22:56:45 -0800

    refactor code in blackjack_v2

---
+++ blackjack/blackjack_v2.py
@@ -0,0 +1,86 @@
+import random
+
+
+def validate_answer(question, valid_answers):
+    while True:
+        answer = input(question).lower()
+        if answer in valid_answers:
+            return answer
+
+
+def yes_or_no(question):
+    return validate_answer(question + ' Type "y" or "n": ', ['y', 'n'])
+
+
+while True:
+    user_cards = []
+    dealer_cards = []
+    deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    start_game = yes_or_no('Would you like to play blackjack?')
+
+    if start_game == 'y':
+        for _ in range(2):
+            user_cards.append(random.choice(deck))
+            dealer_cards.append(random.choice(deck))
+
+    elif start_game == 'n':
+        print('Maybe next time!')
+        exit()
+
+    print(f"Dealer has {dealer_cards[0], '?'}.\n")
+    print(f"You have {user_cards} with a total of {sum(user_cards)}")
+
+    if sum(user_cards) == 21 and len(user_cards) == 2:
+        print('You win with a blackjack!')
+
+    while True:
+        next_step = validate_answer(
+            "Type 'hit' to get another cards or 'stay' to keep your hand as is: ", ['hit', 'stay']
+        )
+
+        if next_step == 'hit':
+            user_cards.append(random.choice(deck))
+            print(f"You have {user_cards} with a total of {sum(user_cards)}")
+            if sum(user_cards) == 21 and len(user_cards) == 2:
+                print('You win with a blackjack!')
+                break
+            if 11 in user_cards and sum(user_cards) > 21:
+                user_cards.remove(11)
+                user_cards.append(1)
+                print('Changing 11 --> 1')
+                print(f'Your hand is now {user_cards} and your total is {sum(user_cards)}')
+            if sum(user_cards) > 21:
+                print('Sorry. Looks like you busted!')
+                break
+
+        elif next_step == 'stay':
+            break
+
+    print(f"Dealer shows {dealer_cards} with a total of {sum(dealer_cards)}")
+
+    while True:
+        if sum(dealer_cards) == 21 and len(dealer_cards) == 2:
+            print("Dealer wins with a blackjack!")
+            break
+        elif sum(dealer_cards) < sum(user_cards):
+            # print_scores(dealer)
+            dealer_cards.append(random.choice(deck))
+            print(f"Dealer draws. {dealer_cards}")
+        elif sum(user_cards) < sum(dealer_cards) <= 21:
+            # print_scores(dealer)
+            print('Dealer wins!')
+            break
+        elif sum(dealer_cards) > 21:
+            # print_scores(dealer)
+            print('Dealer busts. You win!')
+            break
+        elif sum(dealer_cards) == sum(user_cards):
+            # print_scores(dealer)
+            print('Looks like it\'s a draw!')
+            break
+
+    play_again = yes_or_no("Would you like to play again?")
+
+    if play_again == 'n':
+        print('Maybe next time!')
+        exit()
--- blackjack/main.py
+++ blackjack/main.py
@@ -61,6 +61,10 @@ def restart_game():
     play_again = input("Want to play again? Type 'y' for another game or 'n' to stop.").lower()
     if play_again == 'y':
         print('Why won\'t my game restart!?')
+        user_cards = []
+        dealer_cards = []
+        is_playing = False
+        blackjack()
     elif play_again == 'n':
         print('Maybe next time!')
         exit()
@@ -116,109 +120,3 @@ while is_playing:
         print('Oops! Try again.')
 
 restart_game()
-
-
-# First Attempt ###########
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# user_cards = []
-# computer_cards = []
-# user_total = 0
-# computer_total = 0
-# is_playing = False
-#
-#
-# def play_blackjack():
-#     deal_cards()
-#     print(f"Dealer's cards: {computer_cards[0], '?'}\n")
-#     calculate_user_cards(user_total)
-#
-#
-# def pick_random():
-#     random_card = cards[random.randint(0, (len(cards) - 1))]
-#     return random_card
-#
-#
-# def deal_cards():
-#     while len(user_cards) < 2 and len(computer_cards) < 2:
-#         user_cards.append(pick_random())
-#         computer_cards.append(pick_random())
-#
-#
-# def restart():
-#     new_game = input("Want to play again? Type 'y' or 'n': ").lower()
-#     if new_game == 'n':
-#         print('Thanks for playing!')
-#         exit()
-#     elif new_game == 'y':
-#         play_blackjack()
-#
-#
-# def calculate_user_cards(total):
-#     for card in user_cards:
-#         total += card
-#     # If user gets blackjack ###########################
-#     print(f"Your cards are: {user_cards} and your current total is {total}")
-#     if total == 21:
-#         print("Blackjack! You win!")
-#         restart()
-#     elif total > 21:
-#         print("Sorry, you bust!")
-#         restart()
-#     elif total < 21:
-#         hit_or_stay()
-#
-#
-# def hit_or_stay():
-#     next_step = input("Type 'hit' for another card or type 'stay' to keep your hand: ").lower()
-#     if next_step == 'hit':
-#         user_cards.append(pick_random())
-#         calculate_user_cards(user_total)
-#     elif next_step == 'stay':
-#         computer_draw(computer_total, user_total)
-#     else:
-#         print("Please type 'hit' or 'stay'")
-#         hit_or_stay()
-#
-#
-# def computer_draw(total, user_score):
-#     print(f"Dealer reveals: {computer_cards}")
-#     for card in computer_cards:
-#         total += card
-#     print(f"Dealer has a total of {total}")
-#     if total < user_score:
-#         computer_cards.append(pick_random())
-#         computer_draw(total)
-#     elif total == user_score:
-#         print(f"Looks like it's a draw - Let's try again!")
-#         deal_cards()
-#     elif total == 21:
-#         print(f"Dealer has blackjack! Sorry, you lose!")
-#     elif total > 21:
-#         print("Dealer busts. Congrats! You win!")
-#     elif total > user_score:
-#         print(f"Dealer has {total} and you have {user_score}. Sorry, you lose!")
-#         restart()
-#
-#
-# def hit():
-#     user_cards.append(pick_random())
-#     calculate_user_cards(user_total)
-#
-#
-# start_game = input("Would you like to play a game of blackjack?\n"
-#                    "Type 'yes' to start or 'no' to exit\n")
-#
-# if start_game == 'yes':
-#     is_playing = True
-#     play_blackjack()
-#
-#     next_step = input("Type 'hit' for another card or 'stay' to keep your hand: ").lower()
-#     if next_step == 'hit':
-#         hit()
-#
-#     elif next_step == 'stay':
-#         print('run computer draw function')
-#
-# elif start_game == 'no':
-#     is_playing = False
-#     print('Come back when you\'re ready to play some cards!')
