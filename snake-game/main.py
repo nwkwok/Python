56c635af0b0f424c19721d3ee4e1215c90ac43e6
nwkwok <nwkwok@gmail.com>
2021-01-11 22:34:35 -0800

    create snake class

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,26 +8,12 @@ screen.bgcolor('black')
 screen.title('Nick Kwok\'s Snake Game')
 screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-for i in range(3):
-    new_segment = Turtle(shape='square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(starting_position[i])
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
