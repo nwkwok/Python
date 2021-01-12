27e2dbe5f5885e5b2a9651d2997a8f0d3fff2c37
nwkwok <nwkwok@gmail.com>
2021-01-12 00:00:10 -0800

    create food class and scoreboard class

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -9,6 +13,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -16,6 +21,7 @@ class Snake:
             new_segment.color('white')
             new_segment.penup()
             new_segment.goto(position)
+            new_segment.speed('fastest')
             self.segments.append(new_segment)
 
     def move(self):
@@ -24,3 +30,19 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
