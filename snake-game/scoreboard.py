e2263b611d597e9ccaa6f32108a3fbdb1f095230
nwkwok <nwkwok@gmail.com>
2021-01-12 00:20:33 -0800

    add endgame logic

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -17,6 +17,10 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", False, ALIGNMENT, FONT)
+
     def increase_score(self):
         self.score += 1
         self.clear()
