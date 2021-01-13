5373e4cc8d6c12209924fc51b95de49eaba83f17
nwkwok <nwkwok@gmail.com>
2021-01-12 23:29:08 -0800

    commit polls app after completing template

---
+++ django-test/mysite/polls/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # context is what is used to pass in variables to your templates
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    # after importing django.shortcuts, you can grab the render method which
+    # takes in 3 arguments and returns an HttpResponse object
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = 'You\'re looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('You\'re voting on question %s.' % question_id)
