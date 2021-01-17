7a0dd66cf124f45dea3425338d91ef22165c226b
nwkwok <nwkwok@gmail.com>
2021-01-17 14:12:53 -0800

    Create user registration form and add new user feature. Use crispy form and boostrap to style form

--- django-blog/project/blog/views.py
+++ django-blog/project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {
-        'author': 'Nick Kwok',
-        'title': 'Blog Post #1',
-        'content': 'This is my first post',
-        'date_posted': 'January 15th, 2021'
-    },
-    {
-        'author': 'Natalia Kwok',
-        'title': 'Blog Post #2',
-        'content': 'This is the second post on the blog',
-        'date_posted': 'January 16th, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
