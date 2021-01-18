62e4717d77bbe6b00f90f0dd00ccc4e9d1bf2b95
nwkwok <nwkwok@gmail.com>
2021-01-17 22:41:34 -0800

    create Profile model to add profile image, create media root and create signal.py

--- django-blog/project/project/urls.py
+++ django-blog/project/project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -26,3 +28,8 @@ urlpatterns = [
     path('profile/', user_views.profile, name="profile"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
