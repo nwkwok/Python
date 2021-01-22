4cc7283343deb9390240a1e48e602ca14b9454a2
nwkwok <nwkwok@gmail.com>
2021-01-21 21:58:06 -0800

    add pagination and add UserPostListView

--- django-blog/project/blog/urls.py
+++ django-blog/project/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import (PostListView,
+                    UserPostListView,
                     PostDetailView,
                     CreatePostView,
                     PostUpdateView,
@@ -9,6 +10,7 @@ from .views import (PostListView,
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
     path('post/<int:pk>/', PostDetailView.as_view(), name='blog-detail'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='blog-user'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='blog-update'),
     path('post/new/', CreatePostView.as_view(), name='blog-create'),
