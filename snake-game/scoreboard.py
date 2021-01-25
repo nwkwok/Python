b9d13aaf344cbc835c947d87369ad5ffeb8408db
nwkwok <nwkwok@gmail.com>
2021-01-24 23:29:22 -0800

    add high score feature

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(0, 277)
@@ -15,11 +17,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, ALIGNMENT, FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", False, ALIGNMENT, FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", False, ALIGNMENT, FONT)
 
     def increase_score(self):
         self.score += 1
